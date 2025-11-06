8c77ae5ce9859ffe6deecfe07de0680f6b4a0fcb
Xavier Monente <xavier.monente@estudiantat.upc.edu>
2025-11-06 18:01:22 +0000

    Sessio2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-  def hello
-    render html: "<h1>It works WASLAB04!</h1>".html_safe
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_or_create_by!(username: "demo") { |u| u.display_name = "Demo User" }
+  end
+
+  def require_login
+    redirect_to posts_path, alert: "Necesitas estar autenticado." unless current_user
   end
 end
